5aa98c3d71cc1edb96b1ca4cfa663e342f8ae75d
YuhBish <elias.stromberg@elev.ga.ntig.se>
2021-03-10 20:31:41 +0100

    Kap5.5.2.py KLAR

--- Kap5.5.2.py
+++ Kap5.5.2.py
@@ -1,15 +1,20 @@
 import datetime
-import math
 
 dt=datetime.datetime.now()
+d=dt.date()
+dtext=str(d)
 
 print("Vad är ditt personummer? I formatet ÅÅMMDDXXXX ")
 persnmr=input("")
-birthdateM=(persnmr[2] + persnmr[3])
-birthdateD=(persnmr[4] + persnmr[5])
+birthdate=(persnmr[2:6])
 month=(dt.month)
+dtext1=dtext[5]
+dtext2=dtext[6]
+dtext3=dtext[8]
+dtext4=dtext[9]
+dtext0=dtext1+dtext2+dtext3+dtext4 
 
-if month>10:
-    print("0"+(month))
+if dtext0 == birthdate:
+    print("Grattis på födelsedagen!")
 else:
-    print("k")
+    print("Hejdå")
